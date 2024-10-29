0cf6ee52201448fe2d62d5053bc0f5de74513835
Otávio Jacobi <otaviojacobi@gmail.com>
2024-10-29 10:24:46 -0300

    feat: add get and cancel hosted_sessions (#14)

---
+++ lib/docs_web/parameters/hosted_session_id.ex
@@ -0,0 +1,16 @@
+defmodule DocsWeb.Parameters.HostedSessionID do
+  alias OpenApiSpex.{Parameter, Schema}
+  require OpenApiSpex
+
+  @spec parameter() :: Parameter.t()
+  def parameter(),
+    do: %Parameter{
+      name: "hosted_session_id",
+      description: "hosted_session_id parameter",
+      in: :path,
+      required: true,
+      schema: %Schema{
+        type: "string"
+      }
+    }
+end
