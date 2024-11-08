03b18379180b16f1a01ce13613aabe9d748373d4
Otavio Jacobi <otaviojacobi@gmail.com>
2024-11-08 08:30:34 -0300

    fix: remove shipment_id from shipment_exception

--- lib/docs_web/schemas/response/shipment_exception.ex
+++ lib/docs_web/schemas/response/shipment_exception.ex
@@ -1,77 +1,19 @@
 defmodule DocsWeb.Schemas.Response.ShipmentException do
   alias OpenApiSpex.Schema
+  alias DocsWeb.Schemas.Fields
 
   require OpenApiSpex
 
   OpenApiSpex.schema(%{
     title: "ShipmentException",
     type: :object,
-    properties: %{
-      created_at: %Schema{
+    properties:
+      Fields.shipment_exception_base_fields()
+      |> Map.put(:shipment_id, %Schema{
         type: :string,
-        format: "date-time",
-        example: "2024-08-13T22:44:39.593704",
-        description:
-          "A NaiveDatetime-formatted timestamp describing when the exception was created with microsecond precision"
-      },
-      exception_type_label: %Schema{
-        type: :string,
-        description: "An additional label providing context about the exception type (optional)",
-        nullable: true
-      },
-      hold_until: %Schema{
-        type: :string,
-        description: "The date the shipment will be held until",
-        format: "date",
-        nullable: true
-      },
-      id: %Schema{
-        type: :string,
-        description: "The id of the exception in UUID format",
+        description: "The id of the shipment associated with this exception",
         format: :uuid
-      },
-      package_id: %Schema{
-        type: :integer,
-        format: :int64,
-        description: "The package associated with this exception (optional)",
-        example: "12345",
-        nullable: true
-      },
-      resolution: %Schema{
-        type: :string,
-        description: "A brief description of the method by which this exception was resolved",
-        example: "Balance Cleared",
-        nullable: true
-      },
-      shipment_id: %Schema{
-        type: :string,
-        description: "The arta id of the shipment in UUID format",
-        format: :uuid
-      },
-      source: %Schema{
-        type: :string,
-        description: "The source of the exception",
-        example: "arta_team"
-      },
-      status: %Schema{
-        type: :string,
-        description: "The current status for this exception",
-        example: "resolved",
-        enum: ["new", "in_progress", "resolved"]
-      },
-      type: %Schema{
-        type: :string,
-        description: "The type of exception",
-        example: "prepayment_required"
-      },
-      updated_at: %Schema{
-        type: :string,
-        format: "date-time",
-        example: "2024-08-13T22:44:39.593704",
-        description:
-          "A NaiveDatetime-formatted timestamp describing when the exception was last updated with microsecond precision"
-      }
-    },
+      }),
     example: %{
       "created_at" => "2023-05-26T19:33:19.693833",
       "exception_type_label" => nil,
